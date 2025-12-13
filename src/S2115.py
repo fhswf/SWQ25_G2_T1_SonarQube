84e5e8cd2877f85a5d1912f52f7cfe6be04e823c
OesRooster <schelte.peter@fh-swf.de>
2025-12-13 12:36:49 +0100

    Clean up None checks in S2115.py [HAHR, PRSE]
    Removed redundant checks for None in S2115.py.

--- src/S2115.py
+++ src/S2115.py
@@ -16,16 +16,4 @@ def myfunc(param):
 
     if param == None:
         print(param.test())  
-
-    if param is not None:
-        pass
-    else:
-        print(param.test())  
-
-    if param != None:
-        pass
-    else:
-        print(param.test())  
-
-
 #adding something for commit
