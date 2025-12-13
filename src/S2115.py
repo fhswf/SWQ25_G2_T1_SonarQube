b369ae15376e4993a26611dc0f749bf8ea0f1294
OesRooster <schelte.peter@fh-swf.de>
2025-12-13 11:48:58 +0100

    Fix noncompliant connection and return statement [HAHR, PRSE]

--- src/S2115.py
+++ src/S2115.py
@@ -1,11 +1,11 @@
 from mysql.connector import connection
-#connection.MySQLConnection(host='localhost', user='sonarsource', password='')  # Noncompliant
+connection.MySQLConnection(host='localhost', user='sonarsource', password='')  # Noncompliant
 
 
 class MyClass(object):
     def __init__(self):
         self.message = 'Hello'
-        return self  # Noncompliant
+      #  return self  # Noncompliant
 
 
 
